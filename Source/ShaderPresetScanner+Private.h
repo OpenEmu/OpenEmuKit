d1d67b741a37079ae420e0d4cc53544016cafcbf
Stuart Carnie <stuart.carnie@gmail.com>
2021-10-11 07:18:34 +1100

    chore: Fix compiler warning

--- Source/ShaderPresetScanner+Private.h
+++ Source/ShaderPresetScanner+Private.h
@@ -22,7 +22,7 @@
 // (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
 // SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
-#import <OpenEmuKit/ShaderPresetScanner.h>
+#import "ShaderPresetScanner.h"
 
 typedef struct _Scanner {
     /* Scanner state. */
