ddf8763151eeb217ca91ee77c16f09a0aab48c06
Stuart Carnie <stuart.carnie@gmail.com>
2023-08-20 09:26:36 +1000

    chore: Fix compiler warning

--- Source/OEIntegralWindowResizingDelegate.swift
+++ Source/OEIntegralWindowResizingDelegate.swift
@@ -22,7 +22,7 @@
 // (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
 // SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
-import Cocoa
+import AppKit
 
 @objc extension NSWindow {
     final public func maximumIntegralScale(forSize size: NSSize) -> Int {
